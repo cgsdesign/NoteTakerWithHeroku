a1a7d003681bcf651749926fd28f987fbe1c7a28
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-11-14 18:31:28 -0600

    notes now fetchable(get)

--- server.js
+++ server.js
@@ -24,13 +24,37 @@ const app = express();
 app.use(express.static('public'));//links all front end files in to live page client side
 
 
-//only links notes
-app.get('/dbnotes', (req, res) => {
-    console.log("ping me")
+//only links notes (for testing)---------------------------------------------------------
+app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
+//----------------------------------------------------------//
+//------STARTING GET REQUESTS (fetch from API)-------------//
+//--------------------------------------------------------//
 
-//link live pages
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];//NOTE: willhave to add something in case of notes deleted
+    return result;
+  }
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);//findByID for single search paramater
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);//IF NO NOTE/Can't find
+    }
+});
+
+
+//-------------------------------------------//
+//------STARTING POST REQUESTS--------------//
+//-----------------------------------------//
+
+
+//---------------------------------------------------//
+//-----STARTING CODE TO CONNECT WITH LIVE PAGE------//
+//-------------------------------------------------//
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
@@ -38,7 +62,7 @@ app.get('/dbnotes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-//liveify me!
+//liveify me!---------------------------------------------------------
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
